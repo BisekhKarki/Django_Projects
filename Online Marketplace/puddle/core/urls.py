638d76e6076cde2ae8d987423c061e03eff9f74a
BisekhKarki <karkidai79@gmail.com>
2024-12-22 22:27:10 +0545

    Added login and Signup Functionality

--- Online Marketplace/puddle/core/urls.py
+++ Online Marketplace/puddle/core/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.contrib.auth import views as auth_views
+from .forms import loginForm
 
 app_name = 'core'
 
 urlpatterns = [
     path('',views.index,name='index'),
     path('contact/',views.contact,name='contact'),
-    path()
+    path('signup/',views.signup,name="signup"),
+    path('login/',auth_views.LoginView.as_view(template_name='core/login.html',authentication_form=loginForm),name="login"),
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- Online Marketplace/puddle/item/urls.py
+++ Online Marketplace/puddle/item/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
+    path('new/',views.new_item, name='new' ),
     path('<int:pk>/',views.detail,name='detail'),
 ] 
