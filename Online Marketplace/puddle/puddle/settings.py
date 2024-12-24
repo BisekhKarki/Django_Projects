13640c39b0f56f0ca25f4b681565349cbbeeff70
BisekhKarki <karkidai79@gmail.com>
2024-12-24 12:06:35 +0545

    Completed Online Market Place

--- Online Marketplace/puddle/puddle/settings.py
+++ Online Marketplace/puddle/puddle/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'item'
+    'item',
+    'dashboard',
+    'conversation'
 ]
 
 MIDDLEWARE = [
