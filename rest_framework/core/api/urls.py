abec46373cd4ba07733ed313cfea768773afcc03
BisekhKarki <karkidai79@gmail.com>
2025-01-02 12:50:01 +0545

    Rest Framework completed

--- rest_framework/core/api/urls.py
+++ rest_framework/core/api/urls.py
@@ -1,11 +1,21 @@
-from home.views import index,people,login
+from home.views import index,people,login, PersonApi,PeopleViewSet
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'people',PeopleViewSet, basename='people')
+urlpatterns = router.urls
+
 
 
 urlpatterns = [
+
+    path('', include(router.urls)),
     path('index/', index),
     path('person/', people),
     path('login/', login),
+    path('persons/',PersonApi.as_view() ),
 ]
--- rest_framework/core/home/views.py
+++ rest_framework/core/home/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from .models import Person
 from .serializers import PeopleSerializer,LoginSerializer
 from rest_framework.views import APIView 
+from rest_framework import viewsets
+from rest_framework import status
+
 
 @api_view(['GET'])
 def index(request):
@@ -13,6 +16,45 @@ def index(request):
     }
 
     return Response(courses)
+
+
+class PersonApi(APIView):
+    def get(self, request):
+        objs = Person.objects.filter(color__isnull = False)
+        serializer = PeopleSerializer(objs,many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        data = request.data
+        serializer = PeopleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+    def put(self, request):
+        data = request.data
+        serializer = PeopleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def patch(self, request):
+        data = request.data
+        obj = Person.objects.get(id=data['id'])
+        serializer = PeopleSerializer(obj,data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+    def delere(self, request):
+        data = request.data
+        obj = Person.objects.get(id=data['id'])
+        obj.delete()
+        return Response({
+            'message':"Person data deleted"
+        })
+
 
 
 @api_view(['GET','POST','PUT','DELETE'])
@@ -66,3 +108,19 @@ def login(request):
         }) 
 
     return Response(serializer.errors)
+
+
+
+class PeopleViewSet(viewsets.ModelViewSet):
+    serializer_class = PeopleSerializer
+    queryset = Person.objects.all()
+
+    def list(self,request):
+        search = request.GET.get('search')
+        querySet = self.queryset
+        if search:
+            querySet = querySet.filter(name__startswith = search)
+
+        serializer = PeopleSerializer(querySet,many=True)
+
+        return Response({'status':200,'data':serializer.data}, status=status.HTTP_200_OK)
